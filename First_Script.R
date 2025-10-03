7fde9670d7d738d9cc099f79996a0d74e291fc83
Luca-s1 <lucasvieira.art@gmail.com>
2025-10-03 17:26:40 -0300

    Lets go

--- First_Script.R
+++ First_Script.R
@@ -1,1 +1,27 @@
-vamos lá
+library(tidyverse)
+library(palmerpenguins)
+library(ggthemes)
+data("penguins")
+palmerpenguins::penguins
+view(penguins)
+
+ggplot(
+  data = penguins,
+  mapping = aes(x = flipper_length_mm, y = body_mass_g)
+) +
+  geom_point(aes(color = species, shape = species)) +
+  geom_smooth(method = "lm") +
+  labs(
+    title = "Body mass and flipper length",
+    subtitle = "Dimensions for Adelie, Chinstrap, and Gentoo Penguins",
+    x = "Flipper length (mm)", y = "Body mass (g)",
+    color = "Species", shape = "Species"
+  ) 
+  
+ggplot(
+  data = penguins,
+  mapping = aes(x = flipper_length_mm, y = body_mass_g, color = island)
+) +
+  geom_point() +
+  geom_smooth(se = FALSE)
+
